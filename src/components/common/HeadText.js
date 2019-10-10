2353fd36d45030e2d80c108c8af5a42bff7f96ce
Aniruddha Biajdar <aniruddha.ab1@gmail.com>
2019-10-11 03:31:25 +0530

    adde font awsome

---
+++ src/components/common/HeadText.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const HeadText = () => {
+  const activeStyle = { color: "#3b5998" };
+  return (
+
+
+ 
+   <>
+   <h1 className="mt-5">Sticky footer</h1>
+    <p className="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</p>
+    <p>Use <a href="../sticky-footer-navbar/">the sticky footer with a fixed navbar</a> if need be, too.</p>
+   </>
+
+
+
+
+  );
+};
+
+export default HeadText;
+
+
+
+    
--- src/components/courses/SortableList.js
+++ src/components/courses/SortableList.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {SortableContainer, SortableElement, sortableHandle,} from 'react-sortable-hoc';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-const DragHandle = sortableHandle(() => <span>::</span>);
-const SortableItem = SortableElement(({value}) => <li className="list-group-item"><DragHandle/>{value}</li>);
+const DragHandle = sortableHandle(() => <FontAwesomeIcon icon={faBars} /> );
+const SortableItem = SortableElement(({value}) => <li className="list-group-item"><DragHandle/>   {value}</li>);
 
 const SortableList = SortableContainer(({courses}) => {
   return (
